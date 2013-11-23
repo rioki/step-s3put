f8d13f58cb580af9b994aa2826bf3aaa94ddf05f
Sean Farrell <sean.farrell@rioki.org>
2013-11-23 15:11:26 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,32 @@
+# s3put
+
+Upload a file to a s3 bucket. 
+
+Synchronized a directory to a s3 bucket. It makes the bucket identical to the `source-dir`.
+Note that this means that remote files that are not in the `source-dir` are deleted. 
+The synchronized files will get an public access level.
+
+You can use application and deployment variables in wercker.
+
+View this step in the [wercker directory](#TODO)
+
+Current status on wercker:
+
+TODO
+
+## Options
+
+* `key-id` (required) The Amazon Access key that will be used for authorization.
+* `key-secret` (required) The Amazon Access secret that will be used for authorization.
+* `bucket-url` (required) The url of the bucket to sync to, like: `s3://born2code.net`
+* `source-file` (required) The local file to upload to s3.
+* `dest-file` (required) The name of the file to upload to.
+
+## Example
+
+    - s3put:
+        key-id:     $KEY
+        key-secret: $SECRET
+        bucket-url: $BUCKET
+        source-file: myproject-0.1.0.tar.gz
+        dest-file:   myproject-latest.tar.gz
---
+++ run.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+set -e
+cd $HOME
+if [ ! -n "$WERCKER_S3PUT_KEY_ID" ]
+then
+    fail 'missing or empty option key_id, please check wercker.yml'
+fi
+
+if [ ! -n "$WERCKER_S3PUT_KEY_SECRET" ]
+then
+    fail 'missing or empty option key_secret, please check wercker.yml'
+fi
+
+if [ ! -n "$WERCKER_S3PUT_BUCKET_URL" ]
+then
+    fail 'missing or empty option bucket_url, please check wercker.yml'
+fi
+
+if [ ! -n "$WERCKER_S3PUT_SOURCE_FILE" ]
+then
+    fail 'missing or empty option source_file, please check wercker.yml'
+fi
+
+if [ ! -n "$WERCKER_S3PUT_DEST_FILE" ]
+then
+    fail 'missing or empty option dest_file, please check wercker.yml'
+fi
+
+if ! type s3cmd &> /dev/null ;
+then
+    info 's3cmd not found, start installing it'
+    wget -O- -q http://s3tools.org/repo/deb-all/stable/s3tools.key | sudo apt-key add -
+    sudo wget -O/etc/apt/sources.list.d/s3tools.list http://s3tools.org/repo/deb-all/stable/s3tools.list
+    sudo apt-get update && sudo apt-get install s3cmd
+    success 's3cmd installed succesfully'
+else
+    info 'skip s3cmd install, command already available'
+    debug "type s3cmd: $(type s3cmd)"
+fi
+
+if [ -e '.s3cfg' ]
+then
+    warn '.s3cfg file already exists in home directory and will be overwritten'
+fi
+
+echo '[default]' > '.s3cfg'
+echo "access_key=$WERCKER_S3PUT_KEY_ID" >> .s3cfg
+echo "secret_key=$WERCKER_S3PUT_KEY_SECRET" >> .s3cfg
+debug "generated .s3cfg for key $WERCKER_S3PUT_KEY_ID"
+
+source_file="$WERCKER_ROOT/$WERCKER_S3PUT_SOURCE_FILE"
+dest_file="$WERCKER_S3PUT_BUCKET_URL/$WERCKER_S3PUT_DEST_FILE"
+
+info 'starting s3 upload'
+
+# TODO make public an option
+
+set +e
+debug "s3cmd put --acl-public --verbose '$source_file' '$dest_file'"
+sync_output=$(s3cmd sync --acl-public --verbose "$source_file" "$dest_file")
+
+if [[ $? -ne 0 ]];then
+    warning $sync_output
+    fail 's3cmd failed';
+else
+    success 'finished s3 upload';
+fi
+set -e
